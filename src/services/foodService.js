2d7ea8d41a78dbcdf2462297bd5d8012d6202c32
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-11 14:38:38 -0500

    exporting expanded food list

---
+++ src/services/foodService.js
@@ -0,0 +1,7 @@
+export const getAllFood = () => {
+  return fetch(
+    "http://localhost:8088/foods?_expand=type&_expand=quantityUnit&_expand=image&_expand=storage"
+  ).then((res) => {
+    res.json();
+  });
+};
