1fbf0c40bc30470bcce137d34b7785e1c12c0958
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-12 16:01:46 -0500

    Basic routing set up, BrowserRouter added to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { App } from "./App"
-import { createRoot } from "react-dom/client"
-import "./index.css"
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root")
-const root = createRoot(container)
-root.render(<App />)
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
