36634b646685185d00e43f4c3c33ea88fba98876
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-13 13:23:09 -0500

    Fetch functions set for all storage types

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -3,3 +3,21 @@ export const getAllFood = () => {
     "http://localhost:8088/foods?_expand=type&_expand=quantityUnit&_expand=image&_expand=storage"
   ).then((res) => res.json());
 };
+
+export const getPantryFood = () => {
+  return fetch(
+    "http://localhost:8088/foods?_expand=type&_expand=quantityUnit&_expand=image&_expand=storage&storageId=1"
+  ).then((res) => res.json());
+};
+
+export const getFridgeFood = () => {
+  return fetch(
+    "http://localhost:8088/foods?_expand=type&_expand=quantityUnit&_expand=image&_expand=storage&storageId=2"
+  ).then((res) => res.json());
+};
+
+export const getFreezerFood = () => {
+  return fetch(
+    "http://localhost:8088/foods?_expand=type&_expand=quantityUnit&_expand=image&_expand=storage&storageId=3"
+  ).then((res) => res.json());
+};
