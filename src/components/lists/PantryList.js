e08794415420936cfeeccf9c878bcf73d809be3f
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-13 13:50:04 -0500

    PantryList.js added

--- src/components/lists/PantryList.js
+++ src/components/lists/PantryList.js
@@ -1,1 +1,25 @@
 import { useState, useEffect } from "react";
+import { getPantryFood } from "../../services/foodService";
+import "./food.css";
+import { FoodListItem } from "../food/FoodListItem";
+
+export const PantryList = ({ currentUser }) => {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    getPantryFood().then((foodArray) => {
+      setFoods(foodArray);
+    });
+  }, []);
+
+  return (
+    <div className="foods-container">
+      <h2>Your Pantry:</h2>
+      <div className="food-cards-small">
+        {foods.map((food) => {
+          return <FoodListItem food={food} />;
+        })}
+      </div>
+    </div>
+  );
+};
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Navbar } from "../components/nav/Navbar";
 import { FoodForm } from "../components/forms/FoodForm";
 import { FoodList } from "../components/lists/FoodList";
 import { useEffect, useState } from "react";
+import { PantryList } from "../components/lists/PantryList";
 
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({});
@@ -27,6 +28,10 @@ export const ApplicationViews = () => {
       >
         <Route path="new" element={<FoodForm currentUser={currentUser} />} />
         <Route path="all" element={<FoodList currentUser={currentUser} />} />
+        <Route
+          path="pantry"
+          element={<PantryList currentUser={currentUser} />}
+        />
       </Route>
     </Routes>
   );
