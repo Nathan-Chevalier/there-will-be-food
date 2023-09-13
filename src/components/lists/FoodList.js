739e22459ca1bc77eb8a516ecab589aaf26ed2ea
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-13 13:17:00 -0500

    Created FoodListItem component for food list JSX

--- src/components/lists/FoodList.js
+++ src/components/lists/FoodList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getAllFood } from "../../services/foodService";
 import "./food.css";
+import { FoodListItem } from "../food/FoodListItem";
 
 export const FoodList = ({ currentUser }) => {
   const [foods, setFoods] = useState([]);
@@ -13,26 +14,10 @@ export const FoodList = ({ currentUser }) => {
 
   return (
     <div className="foods-container">
-      <h2>Foods:</h2>
+      <h2>All Foods:</h2>
       <div className="food-cards-small">
         {foods.map((food) => {
-          return (
-            <div className="food-card">
-              <div className="food-image">{food.image.name}</div>
-              <div className="food-details-small-a">
-                <h3>{food.name}</h3>
-                <h4>Stored on: {food.storageDate}</h4>
-                <h4>Expires on: {food.expirationDate}</h4>
-              </div>
-              <div>
-                <div className="food-type-small">{food.type.name}</div>
-                <div className="food-storage-small">{food.storage.name}</div>
-                <div className="food-quantity">
-                  {food.quantity} {food.quantityUnit.name}
-                </div>
-              </div>
-            </div>
-          );
+          return <FoodListItem food={food} />;
         })}
       </div>
     </div>
