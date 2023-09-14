ae50e7786ac4c478baefeb9ef0bc0ebdac6d9650
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-14 13:41:34 -0500

    Search added to freezer

--- src/components/lists/FreezerList.js
+++ src/components/lists/FreezerList.js
@@ -5,6 +5,8 @@ import { FoodListItem } from "../food/FoodListItem";
 
 export const FreezerList = ({ currentUser }) => {
   const [foods, setFoods] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredFoods, setFilteredFoods] = useState([]);
 
   useEffect(() => {
     getFreezerFood(currentUser).then((foodArray) => {
@@ -12,11 +14,28 @@ export const FreezerList = ({ currentUser }) => {
     });
   }, [currentUser]);
 
+  useEffect(() => {
+    const foundFood = foods.filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredFoods(foundFood);
+  }, [foods, searchTerm]);
+
   return (
     <div className="foods-container">
       <h2>Your Freezer:</h2>
+      <div>
+        <input
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+          type="text"
+          placeholder="Search Freezer"
+          className="Food Search"
+        />
+      </div>
       <div className="food-cards-small">
-        {foods.map((food) => {
+        {filteredFoods.map((food) => {
           return <FoodListItem food={food} />;
         })}
       </div>
