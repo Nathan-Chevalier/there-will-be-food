41750da0689ed660495892657c74ab440ddca002
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-14 12:00:40 -0500

    Navigation added to item list names

--- src/components/food/FoodListItem.js
+++ src/components/food/FoodListItem.js
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export const FoodListItem = ({ food }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="food-card">
       <div className="food-image">{food.image.name}</div>
       <div className="food-details-small-a">
-        <h3>{food.name}</h3>
+        <h3
+          onClick={() => {
+            navigate(`/food/${food.id}`);
+          }}
+        >
+          {food.name}
+        </h3>
         <h4>Stored on: {food.storageDate}</h4>
         <h4>Expires on: {food.expirationDate}</h4>
       </div>
