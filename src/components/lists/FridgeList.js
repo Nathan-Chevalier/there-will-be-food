fa3acf7eafff13820eef1de6b99b4f9e3c041d6c
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-21 15:20:27 -0500

    flex added to all lists

--- src/components/lists/FridgeList.js
+++ src/components/lists/FridgeList.js
@@ -31,7 +31,7 @@ export const FridgeList = ({ currentUser }) => {
         placeholder="Search Freezer"
         className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
       />
-      <div className="food-cards-small">
+      <div className="flex flex-row flex-wrap justify-around items-start gap-y-5 pt-5">
         {filteredFoods.map((food) => {
           return <FoodListItem food={food} />;
         })}
