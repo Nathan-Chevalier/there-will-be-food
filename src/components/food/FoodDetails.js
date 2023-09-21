25960df437a67788f18acc9167ed6ba4113e79db
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-21 12:46:39 -0500

    Food detail styling

--- src/components/food/FoodDetails.js
+++ src/components/food/FoodDetails.js
@@ -25,36 +25,57 @@ export const FoodDetails = () => {
   };
 
   return (
-    <div className="food-card">
-      <div className="food-image">{food.image?.name}</div>
-      <div className="food-details-small-a">
-        <h3>{food.name}</h3>
-        <h4>Stored on: {food.storageDate}</h4>
-        <h4>Expires on: {food.expirationDate}</h4>
-        <p>Description: {food.description}</p>
-      </div>
-      <div>
-        <div className="food-type-small">{food.type?.name}</div>
-        <div className="food-storage-small">{food.storage?.name}</div>
-        <div className="food-quantity">
-          {food.quantity} {food.quantityUnit?.name}
+    <div className="flex justify-center bg-[#a4bdba] font-helvetica gap-2">
+      <div className="flex bg-[#a47f74] w-8/12 gap-4 p-6 m-5 ">
+        <div className="flex flex-col">
+          <div className="food-image">
+            <img src={food.image?.address} alt={food.image?.name} />
+          </div>
+          <div className="flex flex-col">
+            <button
+              className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-900 hover:border-red-500 rounded"
+              onClick={() => {
+                handleDeleteFood();
+              }}
+            >
+              Delete
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-500 rounded"
+              onClick={() => {
+                navigate(`/food/${foodId}/edit`);
+              }}
+            >
+              Edit
+            </button>
+          </div>
         </div>
-        <button
-          className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-900 hover:border-red-500 rounded"
-          onClick={() => {
-            handleDeleteFood();
-          }}
-        >
-          Delete
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-500 rounded"
-          onClick={() => {
-            navigate(`/food/${foodId}/edit`);
-          }}
-        >
-          Edit
-        </button>
+        <div className="information-container flex flex-col flex-1 justify-between">
+          <div>
+            {" "}
+            <div className="flex justify-between">
+              <h3>{food.name}</h3>
+              <div className="flex">
+                <div className="food-type-small">{food.type?.name}</div>
+                <div className="food-storage-small">{food.storage?.name}</div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div>Description: </div>
+              <div>{food.description}</div>
+            </div>
+          </div>
+
+          <div>
+            <h4 className="">Stored on: {food.storageDate}</h4>
+            <div className="flex justify-between">
+              <h4>Expires on: {food.expirationDate}</h4>
+              <div className="food-quantity">
+                {food.quantity} {food.quantityUnit?.name}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
