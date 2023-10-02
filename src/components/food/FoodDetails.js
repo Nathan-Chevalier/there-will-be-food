5aa8002f3d7b286d686162a30e92bfe89cc64d19
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-02 14:12:02 -0500

    Styling for details

--- src/components/food/FoodDetails.js
+++ src/components/food/FoodDetails.js
@@ -26,68 +26,77 @@ export const FoodDetails = () => {
 
   return (
     <div className="flex justify-center bg-[#a4bdba] font-helvetica gap-2">
-      <div className="flex bg-[#e8d7b1] w-7/12 gap-2 p-6 my-2 rounded-3xl border-white border-8">
-        <div className="flex flex-col justify-between">
-          <div className="food-image">
-            <img src={food.image?.address} alt={food.image?.name} />
+      <div className="flex bg-[#e8d7b1] w-7/12 mt-20 p-4 rounded-3xl border-white border-8">
+        <div className="flex bg-[#bd956d] w-full gap-2 p-6 my-2 rounded-3xl border-white border-8">
+          <div className="flex flex-col justify-between">
+            <div className="food-image">
+              <img src={food.image?.address} alt={food?.image?.name} />
+            </div>
+            <div className="flex flex-col gap-4">
+              <button
+                className="btn-save"
+                onClick={() => {
+                  navigate(`/food/${foodId}/edit`);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-[#bd6d6d] hover:bg-[#994040] text-white font-bold py-2 px-4 border-4 border-white hover:border-white rounded-2xl"
+                onClick={() => {
+                  handleDeleteFood();
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <button
-              className="btn-save"
-              onClick={() => {
-                handleDeleteFood();
-              }}
-            >
-              Delete
-            </button>
-            <button
-              className="btn-save"
-              onClick={() => {
-                navigate(`/food/${foodId}/edit`);
-              }}
-            >
-              Edit
-            </button>
-          </div>
-        </div>
-        <div className="information-container flex flex-col flex-1 justify-between">
-          <div className="flex justify-between flex-col flex-1">
-            <div className="flex justify-between pb-6">
-              <div className="text-5xl text-yellow-400 shadow-text">
-                {food.name}
-              </div>
-              <div className="flex justify-center items-center">
-                <div className="bg-green-600 py-0.5 px-2 text-amber-100 drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] rounded-lg">
-                  <span className="shadow-text">{food.type?.name}</span>
+          <div className="information-container flex flex-col flex-1 justify-between">
+            <div className="flex justify-between flex-col flex-1">
+              <div className="flex justify-between pb-6">
+                <div className="text-4xl text-white font-semibold px-6 ml-6 w-max py-4 rounded-b-lg border-t-3 border-white border-4 mx-2 -translate-y-7 bg-slate-950/10">
+                  {food.name}
                 </div>
-                <div className="bg-green-600 py-0.5 px-2 text-white drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] rounded-lg">
-                  <span className="shadow-text">{food.storage?.name}</span>
+                <div className="flex  flex-col justify-center items-end">
+                  <div className="bg-[#14373b] font-semibold py-0.5 px-2 text-white drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] rounded-lg mb-2">
+                    <span className="shadow-text">{food.type?.name}</span>
+                  </div>
+                  <div className="bg-[#1c0702] py-0.5 px-2 font-semibold text-white drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] rounded-lg my-2">
+                    <span className="shadow-text">{food.storage?.name}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col p-2">
+                <div className="bg-[#bd956d] px-2 py-1 rounded-t-lg w-max translate-x-2 border-white border-2 border-b-0 text-white font-semibold">
+                  Description:{" "}
+                </div>
+                <div className="h-20 rounded-lg bg-[#f3dbc2] border-white border-2 p-2">
+                  <span className="m-2">{food?.description}</span>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col bg-slate-950/10 rounded-xl p-2 border-slate-950/20 border-2">
-              <div className="shadow-text pb-2">Description: </div>
-              <div className="h-20 rounded-lg bg-slate-950/10 border-slate-950/20 border-2 py-1">
-                <span className="m-2">{food.description}</span>
-              </div>
-            </div>
-          </div>
 
-          <div>
-            <div className="flex justify-between rounded-lg bg-slate-950/10 border-slate-950/20 border-2 py-4 px-2">
-              <div className="flex rounded-lg bg-slate-950/10 border-slate-950/20 border-2 py-4 px-2">
-                <h4 className="pr-8">
-                  <span className="shadow-text">Stored on: </span>
-                  {food.storageDate}
-                </h4>
-                <h4>
-                  <span className="shadow-text">Expires on: </span>
-                  {food.expirationDate}
-                </h4>
-              </div>
+            <div>
+              <div className="flex justify-between px-2 pt-3">
+                <div className="flex">
+                  <div>
+                    <span className="bg-[#bd956d] px-2 py-1 rounded-l-lg border-white border-2 border-r-0 text-white font-semibold">
+                      Expires on:{" "}
+                    </span>
+                    <span className="bg-[#f3dbc2] px-2 py-1 rounded-r-lg border-white border-2 font-extrabold text-[#1c0702]">
+                      {food.expirationDate}
+                    </span>
+                  </div>
+                </div>
 
-              <div className="self-center rounded-lg bg-slate-950/10 border-slate-950/20 border-2 py-4 px-2">
-                {food.quantity} {food.quantityUnit?.name}
+                <div className="flex items-center justify-end">
+                  <span className="bg-[#f3dbc2] px-2 py-1 rounded-l-lg border-white border-2 font-extrabold text-[#1c0702]">
+                    {food.quantity}
+                  </span>
+                  <span className="bg-[#bd956d] px-2 py-1 rounded-r-lg border-white border-2 border-l-0 text-white font-semibold">
+                    {food.quantityUnit?.name}
+                  </span>
+                </div>
               </div>
             </div>
           </div>
